82ebd820999eb2da55d39bc309a4705de25c59b3
Samuel Chow <samuel.chow.45@gmail.com>
2025-07-16 09:51:28 -0400

    remove dev files

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+set -e
 
 if [ "$1" == "reset" ]; then
     echo "Resetting Docker containers and volumes..."
-    cd config || exit 1
     sudo docker compose down -v
-    cd ..
     rm -rf autograder2/.first_log
 else
     git submodule update --init --recursive
 
-    cd config || exit 1
     sudo docker compose up --build
 fi
 
