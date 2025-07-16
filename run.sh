e147d581f1aa9f995d1249f8b26051a05fb8a62a
Samuel Chow <samuel.chow.45@gmail.com>
2025-07-16 13:54:33 -0400

    update path

--- run.sh
+++ run.sh
@@ -4,10 +4,9 @@ set -e
 if [ "$1" == "reset" ]; then
     echo "Resetting Docker containers and volumes..."
     sudo docker compose down -v
-    rm -rf autograder2/.first_log
+    rm -rf autograder2/dev/.first_log
 else
     git submodule update --init --recursive
-
     sudo docker compose up --build
 fi
 
