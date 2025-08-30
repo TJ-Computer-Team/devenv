98c8a910e6f4b6977521f8d5901f2f3429eeb090
Zain Marshall <zainmarshall1000@gmail.com>
2025-08-30 18:19:18 -0400

    tmux

--- run.sh
+++ run.sh
@@ -6,8 +6,11 @@ if [ "$1" == "reset" ]; then
     sudo docker compose down -v
     rm -rf autograder2/dev/.first_log
 else
-    git submodule update --init --recursive
-    sudo docker compose build  && sudo docker compose up
+    tmux kill-session -t autograder || true
+    tmux new-session -d -s autograder -n main
+    tmux send-keys -t autograder:0 'docker compose build && docker compose up' C-m
+    tmux split-window -h -t autograder:0
+    tmux send-keys -t autograder:0.1 'cd autograder2/frontend && npm run dev' C-m
 fi
 
 
